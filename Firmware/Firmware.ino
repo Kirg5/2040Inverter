8e9695721f6d04bf04c28e76aa989cede7179760
Kirb <71050041+Kirg5@users.noreply.github.com>
2023-10-21 16:12:19 -0600

    Fix incorrect PWM frequency

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,138 +1,201 @@
-#include <math.h>
-#include <uart.h>
+#include <RP2040_PWM.h>
+#include "Sine_LUT.h"
+#include "Notes.h"
+
+RP2040_PWM* PWM_Instance[6];
 
 // MOSFET pins
-const int phaseAGateLowPin = 18;
-const int phaseBGateLowPin = 19;
-const int phaseCGateLowPin = 20;
-const int phaseAGateHighPin = 23;
-const int phaseBGateHighPin = 22;
-const int phaseCGateHighPin = 21;
+  const int phaseAGateLowPin = 18;
+  const int phaseBGateLowPin = 19;
+  const int phaseCGateLowPin = 20;
+  const int phaseAGateHighPin = 23;
+  const int phaseBGateHighPin = 22;
+  const int phaseCGateHighPin = 21;
+  
+  // Hall sensor pins
+  const int hallSensorAPin = 13;
+  const int hallSensorBPin = 14;
+  const int hallSensorCPin = 15;
 
-// Hall sensor pins
-const int hallSensorAPin = 13;
-const int hallSensorBPin = 14;
-const int hallSensorCPin = 15;
+  // ADC pins
+  const int currentSensePin = 26;
+  const int voltageSensePin = 27;
+  const int tempSensePin = 28;
+  const int ambientTempSensePin = 29;
 
-// ADC pins
-const int currentSensePin = 26;
-const int voltageSensePin = 27;
-const int tempSensePin = 28;
-const int ambientTempSensePin = 29;
+  // Misc pins
+  const int ledR = 4;
+  const int ledG = 5;
+  const int ledB = 6;
+  const int Fan = 7;
 
-// UART Data pins
-const int uartTX = 8;
-const int uartRX = 9;
+  // Vars
+  int currentAngle = 0;
+  int voltageRaw = 0;
+  int currentRaw = 0;
+  float voltage = 0;
+  float current = 0;
+  uint32_t freqSwitching = 8000;
+  int PWM = 60;
+  int PWMMode = 1; 
 
-// Misc system Pins
-const int fanPin = 7;
-const int ledR = 4;
-const int ledG = 5;
-const int ledB = 6;
-const int ledInternal = 25;
-const int VOC = 24;
-const int extGPIO = 17;
+  //PWMMode 1  = Square wave, static frequency
+  //PWMMode 2  = Square wave, dynamic frequency
+  //PWMMode 3  = Stepped sine wave, static frequency
+  //PWMMode 4  = Stepped sine wave, dynamic frequency 
+  //PWMMode 5  = Sine wave, static frequency
+  //PWMMode 6  = Sine wave, dynamic frequency
+  //PWMMode 7  = 7 pulse synchronous sine
+  //PWMMode 8  = 5 pulse synchronous sine
+  //PWMMode 9  = 3 pulse synchronous sine
+  //PWMMode 10 = Square wave tone
+  
+  void setup() {
+  analogWrite(Fan, 255);
 
-// System parameters
-const int motorPolePairs = 46;        // Number of pole pairs in the motor, used to calculate RPM
-const int maxForwardCurrent = 48;     // Motor current limit in amps
-const int maxReverseCurrent = 10;     // Regen current limit in amps
-const float undervolageCutoff = 3.2;  // Minimum cell voltage to power the motor in volts
-const int parallelGroupCount = 20;    // How many cells are in parallel in the battery pack
-const float
+  // LED pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
 
-// Control variables
-float targetPower = 0;                // Target output power set by the user
-float currentAngle = 0;               // Current electrical angle of the motor
-float errorSum = 0;                   // Error sum for integral control
-float prevError = 0;                  // Previous error for derivative control
+  //Initialize High Side PWM Channels
+  PWM_Instance[1] = new RP2040_PWM(phaseAGateHighPin, freqSwitching, 0);
+  PWM_Instance[2] = new RP2040_PWM(phaseBGateHighPin, freqSwitching, 0);
+  PWM_Instance[3] = new RP2040_PWM(phaseCGateHighPin, freqSwitching, 0);
 
-// Control constants
-const float Kp = 0.01;                // Proportional gain
-const float Ki = 0.001;               // Integral gain
-const float Kd = 0.001;               // Derivative gain
-
-// Sine lookup table
-const int numAngles = 360;
-const float angleStep = 360.0 / numAngles;
-float sinTable[numAngles];
-
-void setup() {
-  // Set the pins as outputs for high-side MOSFET gate drives
-  pinMode(phaseAGateHighPin, OUTPUT);
-  pinMode(phaseBGateHighPin, OUTPUT);
-  pinMode(phaseCGateHighPin, OUTPUT);
-
-  // Set the pins as outputs for low-side MOSFET gate drives
-  pinMode(phaseAGateLowPin, OUTPUT);
-  pinMode(phaseBGateLowPin, OUTPUT);
-  pinMode(phaseCGateLowPin, OUTPUT);
+  //Initialize Low Side PWM Channels
+  PWM_Instance[4] = new RP2040_PWM(phaseAGateLowPin, freqSwitching, 0);
+  PWM_Instance[5] = new RP2040_PWM(phaseBGateLowPin, freqSwitching, 0);
+  PWM_Instance[6] = new RP2040_PWM(phaseCGateLowPin, freqSwitching, 0);
 
   // Set the pins as inputs for hall effect sensors
   pinMode(hallSensorAPin, INPUT);
   pinMode(hallSensorBPin, INPUT);
   pinMode(hallSensorCPin, INPUT);
 
-  //Initialize UART communication
-  #define UART_ID uart0
-  #define BAUD_RATE 115200
-  #define DATA_BITS 40
-  #define STOP_BITS 1
-  #define PARITY    UART_PARITY_NONE
+  // Setup ADC
+  pinMode(currentSensePin, INPUT);
+  pinMode(voltageSensePin, INPUT);
 
-  // Populate the sine lookup table
-  for (int i = 0; i < numAngles; i++) {
-    float radian = i * angleStep * PI / 180.0;
-    sinTable[i] = sin(radian);
-  }
+  // Initialize serial
+  Serial.begin(9600);
+
+  delay(100);
+  analogWrite(Fan, 0);
 }
 
-void loop() {
-  // Read the state of hall effect sensors
+void loop() 
+  {
+  // Read raw current and voltage values
+  int currentRaw = analogRead(currentSensePin);
+  int voltageRaw = analogRead(voltageSensePin);
+
+  // Calculate actual voltage and current
+  current = (currentRaw - 511.5) * 50 / 511.5;
+  voltage = voltageRaw * 108.9 / 1023;
+
+  // Read hall sensors
   int hallSensorAState = digitalRead(hallSensorAPin);
   int hallSensorBState = digitalRead(hallSensorBPin);
   int hallSensorCState = digitalRead(hallSensorCPin);
 
-  // Calculate the electrical angle based on the hall sensor states
-  if (hallSensorAState == HIGH && hallSensorBState == LOW && hallSensorCState == LOW) {
+  // Calculate electrical angle
+         if (hallSensorAState == HIGH && hallSensorBState == LOW && hallSensorCState == HIGH) {
     currentAngle = 0;
-  } else if (hallSensorAState == HIGH && hallSensorBState == LOW && hallSensorCState == HIGH) {
+  } else if (hallSensorAState == HIGH && hallSensorBState == LOW && hallSensorCState == LOW) {
     currentAngle = 60;
-  } else if (hallSensorAState == LOW && hallSensorBState == HIGH && hallSensorCState == HIGH) {
+  } else if (hallSensorAState == HIGH && hallSensorBState == HIGH && hallSensorCState == LOW) {
     currentAngle = 120;
   } else if (hallSensorAState == LOW && hallSensorBState == HIGH && hallSensorCState == LOW) {
     currentAngle = 180;
-  } else if (hallSensorAState == HIGH && hallSensorBState == HIGH && hallSensorCState == LOW) {
+  } else if (hallSensorAState == LOW && hallSensorBState == HIGH && hallSensorCState == HIGH) {
     currentAngle = 240;
-  } else if (hallSensorAState == HIGH && hallSensorBState == LOW && hallSensorCState == LOW) {
+  } else if (hallSensorAState == LOW && hallSensorBState == LOW && hallSensorCState == HIGH) {
     currentAngle = 300;
   }
 
-  // Calculate the index in the sine lookup table
-  int index = static_cast<int>(currentAngle) % numAngles;
+  //Motor commutation
+  if (PWMMode == 1) {
+    if (currentAngle == 0) {
+      PWM_Instance[4]->setPWM(phaseAGateLowPin, freqSwitching, 0);
+      PWM_Instance[5]->setPWM(phaseBGateLowPin, freqSwitching, PWM);
+      PWM_Instance[6]->setPWM(phaseCGateLowPin, freqSwitching, 0);
 
-  // Get the sine value from the lookup table
-  float sinValue = sinTable[index];
+      PWM_Instance[1]->setPWM(phaseAGateHighPin, freqSwitching, PWM);
+      PWM_Instance[2]->setPWM(phaseBGateHighPin, freqSwitching, 0);
+      PWM_Instance[3]->setPWM(phaseCGateHighPin, freqSwitching, 0);
+    } else if (currentAngle == 60) {
+      PWM_Instance[4]->setPWM(phaseAGateLowPin, freqSwitching, 0);
+      PWM_Instance[5]->setPWM(phaseBGateLowPin, freqSwitching, 0);
+      PWM_Instance[6]->setPWM(phaseCGateLowPin, freqSwitching, PWM);
 
-  //Get throttle value from other core
-  int throttle = idk
+      PWM_Instance[1]->setPWM(phaseAGateHighPin, freqSwitching, PWM);
+      PWM_Instance[2]->setPWM(phaseBGateHighPin, freqSwitching, 0);
+      PWM_Instance[3]->setPWM(phaseCGateHighPin, freqSwitching, 0);
+    } else if (currentAngle == 120) {
+      PWM_Instance[4]->setPWM(phaseAGateLowPin, freqSwitching, 0);
+      PWM_Instance[5]->setPWM(phaseBGateLowPin, freqSwitching, 0);
+      PWM_Instance[6]->setPWM(phaseCGateLowPin, freqSwitching, PWM);
 
-  // Clip the throttle value within the valid range
-  throttle = constrain(throttle, 0, maxPWM);
+      PWM_Instance[1]->setPWM(phaseAGateHighPin, freqSwitching, 0);
+      PWM_Instance[2]->setPWM(phaseBGateHighPin, freqSwitching, PWM);
+      PWM_Instance[3]->setPWM(phaseCGateHighPin, freqSwitching, 0);
+    } else if (currentAngle == 180) {
+      PWM_Instance[4]->setPWM(phaseAGateLowPin, freqSwitching, PWM);
+      PWM_Instance[5]->setPWM(phaseBGateLowPin, freqSwitching, 0);
+      PWM_Instance[6]->setPWM(phaseCGateLowPin, freqSwitching, 0);
 
-  // Apply control signal to the motor phases using sinusoidal control
-  int pwmA = throttle * (sinValue + 0.5);
-  int pwmB = throttle * (sinValue - 0.5);
-  int pwmC = -pwmA - pwmB;
+      PWM_Instance[1]->setPWM(phaseAGateHighPin, freqSwitching, 0);
+      PWM_Instance[2]->setPWM(phaseBGateHighPin, freqSwitching, PWM);
+      PWM_Instance[3]->setPWM(phaseCGateHighPin, freqSwitching, 0);
+    } else if (currentAngle == 240) {
+      PWM_Instance[4]->setPWM(phaseAGateLowPin, freqSwitching, PWM);
+      PWM_Instance[5]->setPWM(phaseBGateLowPin, freqSwitching, 0);
+      PWM_Instance[6]->setPWM(phaseCGateLowPin, freqSwitching, 0);
 
-  // Apply the PWM values to the gate drives
-  analogWrite(phaseAGateHighPin, pwmA);
-  analogWrite(phaseAGateLowPin, pwmA);    // Set low-side gate drive equal to high-side for current control
-  analogWrite(phaseBGateHighPin, pwmB);
-  analogWrite(phaseBGateLowPin, pwmB);
-  analogWrite(phaseCGateHighPin, pwmC);
-  analogWrite(phaseCGateLowPin, pwmC);
+      PWM_Instance[1]->setPWM(phaseAGateHighPin, freqSwitching, 0);
+      PWM_Instance[2]->setPWM(phaseBGateHighPin, freqSwitching, 0);
+      PWM_Instance[3]->setPWM(phaseCGateHighPin, freqSwitching, PWM);
+    } else if (currentAngle == 300) {
+      PWM_Instance[4]->setPWM(phaseAGateLowPin, freqSwitching, 0);
+      PWM_Instance[5]->setPWM(phaseBGateLowPin, freqSwitching, PWM);
+      PWM_Instance[6]->setPWM(phaseCGateLowPin, freqSwitching, 0);
 
-  // Add a suitable delay between motor control updates
-  delay(10);
+      PWM_Instance[1]->setPWM(phaseAGateHighPin, freqSwitching, 0);
+      PWM_Instance[2]->setPWM(phaseBGateHighPin, freqSwitching, 0);
+      PWM_Instance[3]->setPWM(phaseCGateHighPin, freqSwitching, PWM);
+    }
+  } else if (PWMMode == 2) { // Same as previous mode but dynamic freq
+
+  } else if (PWMMode == 3) { // Mode 1 but 1/2 PWM
+
+  } else if (PWMMode == 4) { // Same as previous mode but dynamic freq
+
+  } else if (PWMMode == 5) { // Full sine control
+
+  } else if (PWMMode == 6) { // Same as previous mode but dynamic freq
+
+  } else if (PWMMode == 7) { // Multipulse mode
+
+  } else if (PWMMode == 8) { // Multipulse mode again
+
+  } else if (PWMMode == 9) { // Multipulse mode yet again
+
+  } else if (PWMMode == 10) { // Similar to mode 2 but using tones
+
+  }
+if (Serial.available() > 0) {
+  PWM = 250;
+}
+
+analogWrite(LED_BUILTIN, PWM);
+
+Serial.print("angle = ");
+Serial.print(currentAngle);
+Serial.print("\t voltage = ");
+Serial.print(voltage);
+Serial.print("\t current = ");
+Serial.println(current);
+  delay(0);
 }
